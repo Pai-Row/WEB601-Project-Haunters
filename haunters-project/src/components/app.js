b0851aac37e933fd27e0bbaa9222d80def8671ea
Jonathan Macgregor <52035222+DualWielder@users.noreply.github.com>
2019-08-26 10:51:17 +1200

    Fixes

--- haunters-project/src/components/app.js
+++ haunters-project/src/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import ReactDOM from 'react-dom'
 
 import Home from './pages/Home'
 import Attractions from './pages/Attractions'
