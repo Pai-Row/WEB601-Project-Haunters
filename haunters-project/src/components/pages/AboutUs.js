14de0bcf7b00d41526cc79a6d75d25f7fedc1fad
Jonathan Macgregor <52035222+DualWielder@users.noreply.github.com>
2019-08-26 16:13:53 +1200

    First details added to the website

--- haunters-project/src/components/pages/AboutUs.js
+++ haunters-project/src/components/pages/AboutUs.js
@@ -3,6 +3,10 @@ import React from 'react'
 const AboutUs = () => {
     return <div>
         <h1> About Us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies metus urna, vitae interdum sapien blandit tincidunt. Donec tincidunt condimentum purus, ac hendrerit nisi venenatis eget. Aliquam nec justo in tellus aliquam pretium sit amet eget dui. In blandit mauris eget ligula tempor, eu volutpat dolor consectetur. Donec tempus urna quis sem pulvinar, et posuere sem volutpat. Vivamus id pharetra sem, a molestie libero. Suspendisse potenti. Praesent et orci ut felis cursus lobortis. Curabitur ut dapibus lorem. Quisque dapibus, sapien id tincidunt aliquam, tellus lorem iaculis ante, in vestibulum felis turpis a nisi.</p>
+        <h2>Contact the Otherside</h2>
+        <h2>Directions</h2>
+        <h2>F.A.Q</h2>
         </div>
 }
 
