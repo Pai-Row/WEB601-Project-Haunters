a98c0290aae2424dc4c83cc368614abd87a67d59
Jono <jonomacgregor@gmail.com>
2019-10-16 10:51:18 +1300

    Colour changes to navbar

--- haunters-project/src/components/NavBar.js
+++ haunters-project/src/components/NavBar.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <div className="ui  pointing right menu" style={{backgroundColor: 'DarkBlue'}}>
-            <Link to="/" className="item"  style={{backgroundColor: 'lightGrey'}}>
+        <div className="ui  pointing right menu" style={{backgroundColor: 'Dark'}}>
+            <Link to="/" className="item"  style={{backgroundColor: 'Dark'}}>
                 Home
             </Link>
             <div className="right menu">
-                <Link to="/attractions" className="item" style={{backgroundColor: 'lightGrey'}}>
+                <Link to="/attractions" className="item" style={{backgroundColor: 'Dark'}}>
                     Attractions
                 </Link>
                 <div className="right menu">
-                <Link to="/booking" className="item" style={{backgroundColor: 'lightGrey'}}>
+                <Link to="/booking" className="item" style={{backgroundColor: 'Dark'}}>
                     Booking
                 </Link>
                 <div className="right menu">
-                <Link to="/media" className="item" style={{backgroundColor: 'lightGrey'}}>
+                <Link to="/media" className="item" style={{backgroundColor: 'Dark'}}>
                     Media
                 </Link>
                     
                     <div className="right menu">
-                <Link to="/aboutus" className="item" style={{backgroundColor: 'lightGrey'}}>
+                <Link to="/aboutus" className="item" style={{backgroundColor: 'Dark'}}>
                     About Us
                 </Link>
                 </div>
