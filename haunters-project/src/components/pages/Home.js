f418b792d032cf41c0c403be8a135d3e5f940a2b
Jonathan Macgregor <52035222+DualWielder@users.noreply.github.com>
2019-10-16 15:32:13 +1300

    Fixing main image

--- haunters-project/src/components/pages/Home.js
+++ haunters-project/src/components/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <center><p>A frightenly wonderful way to spend your evening...</p></center>
         <br></br>
         <div className="content-image" >
-         <center><img className="image" src=".\images\House.jpg" alt="Haunters House" /></center>
+         <center><img className="image-medium" src=".\images\House.jpg" alt="Haunters House" /></center>
           </div>
         <center><p>Haunters is a newly opened horror theme attraction park filled to the brim with frights and fun... <br></br>If you love to be scared or are just looking for something different and unqiue then come on in and give us a visit<br></br> the temperature is chilling...</p></center>
 
